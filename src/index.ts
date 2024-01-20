5043963d8c7b500e994f368e9947bda26890ff14
Harshit <harshit@coastr.com>
2024-01-20 21:31:12 +0530

    remove unwanted comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { ErrorRequestHandler, NextFunction } from "express";
+import express, { NextFunction } from "express";
 import { config } from "./config";
 import { routes } from "./routes";
 import cors from "cors";
@@ -7,22 +7,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.get("/", (req, res: any) => {
-//   res.status(200).response({
-//     success: true,
-//     message: "Apparel updated successfully!",
-//   });
-// })
-// app.use((err: any, req: any, res: any, next: NextFunction): void => {
-//   console.log("middleware running")
-//   next()
-// });
-
-
-// app.get("/status", (req: any, res: any) => {
-//   console.log("reached")
-//   res.status(200).end()
-// });
 
 app.use("/", routes);
 
