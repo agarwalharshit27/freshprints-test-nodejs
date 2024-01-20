fa885007d7ace6886c862e58f9f4ab1314cb6d1c
Harshit <harshit@coastr.com>
2024-01-20 21:32:16 +0530

    fix typo

--- src/components/apparel/apparel.routes.ts
+++ src/components/apparel/apparel.routes.ts
@@ -8,11 +8,10 @@ apparelRoutes.use("/apparel", apparelRoutes)
 apparelRoutes.post('/inventory-update',  async (req: any, res: any, next: NextFunction) => {
   try {
     const data = req.body.data;
-    const apparel = await updateApparelInventory(data);
+    const response = await updateApparelInventory(data);
     res.status(200).send({
       success: true,
       message: "Apparel updated successfully!",
-      data: apparel
     })
     next()
   } catch (e) {
